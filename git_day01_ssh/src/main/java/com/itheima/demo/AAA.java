b9eda6b74d87397bc1cf62ee01ddf44b3c51432e
liuleilei001 <42605240+liuleilei001@users.noreply.github.com>
2019-02-19 17:41:09 +0800

    ssh测试

---
+++ git_day01_ssh/src/main/java/com/itheima/demo/AAA.java
@@ -0,0 +1,15 @@
+package com.itheima.demo;
+
+import sun.applet.Main;
+
+/**
+ * @Description TODO
+ * @Author llei
+ * @Date 2019/2/19 17:37
+ * @Version v1.0.0
+ **/
+public class AAA {
+    public static void main(String[] args) {
+        System.out.println("你好!");
+    }
+}
